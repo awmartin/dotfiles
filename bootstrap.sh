34767eacbf9fab7367bb02100a5509aee3a0eb56
William Martin <awmartin@gmail.com>
2016-07-31 19:07:48 -0400

    Separated config for bash and zsh
    * Assumes a user may already have .bashrc, .bash_profile, or .zshrc.
    * Appends sourcing the dotfiles customization to each.
    * Downloads the latest zshrc template.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,90 @@
 #!/bin/sh
 
-# Creates symlinks from $HOME into dotfiles.
-# TODO: Maybe this should append source commands to existing files instead?
+echo
+echo "Starting bootstrap process..."
+echo
 
 dotfiles_dir="$HOME/dotfiles"
-dotfiles_home_dir="$dotfiles_dir/home"
 
+# First, append all the bash scripts to existing bash scripts if found.
+dotfiles_bash_dir="$dotfiles_dir/bash"
+
+bash_files=( "bash_profile" "bashrc" )
+for file in $bash_files; do
+  if [ -f "$HOME/.$file" ]; then
+    # If the file exists, source the dotfiles file at the end.
+    echo "Appending: source \"dotfiles/bash/$file\" to ~/.$file"
+    echo -e "\nsource \"$dotfiles_bash_dir/$file\"" >> "$HOME/.$file"
+  else
+    # Otherwise, copy it from dotfiles/bash.
+    echo "~/.$file didn't exist. Copying from dotfiles/bash/$file"
+    cp "$dotfiles_bash_dir/$file" "$HOME/.$file"
+  fi
+done
+
+echo
+
+
+# Zshell is similar, but include a template .zshrc if it doesn't already exist.
+dotfiles_zsh_dir="$HOME/dotfiles/zsh"
+
+if [ ! -f "$HOME/.zshrc" ]; then
+  echo "~/.zshrc not found. Getting template from github.com/robbyrussell/oh-my-zsh"
+  curl -s "https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/templates/zshrc.zsh-template" > $HOME/.zshrc
+fi
+
+# And append sourcing the contents of the customizations.
+echo "Appending: source \"dotfiles/zsh/zshrc\" to .zshrc"
+echo -e "\nsource \"$dotfiles_zsh_dir/zshrc\"" >> $HOME/.zshrc
+echo
+
+
+# Links a subset of folders to $HOME.
 toplevel_folders_link_without_dots=( "bin" )
-toplevel_folders_link_with_dots=( "vim" "oh-my-zsh" "env" "shell" )
-files_link_with_dots=`ls -1 $dotfiles_home_dir`
 
 for folder in $toplevel_folders_link_without_dots; do
   # Back up the existing folder.
   if [ -d "$HOME/$folder" ]; then
-    mv "$HOME/$folder" "$HOME/$folder.backup"
+    echo "Backing up ~/$folder"
+    mv "$HOME/$folder" "$HOME/$folder.bak"
   fi
 
+  echo "Symlinking ~/$folder to dotfiles/$folder"
   ln -s "$dotfiles_dir/$folder" "$HOME/$folder"
 done
+
+echo
+
+
+# Links a subset of folders to their dot equivalents in $HOME.
+toplevel_folders_link_with_dots=( "vim" "oh-my-zsh" "env" "shell" "bash-it" )
 
 for folder in $toplevel_folders_link_with_dots; do
   # Back up the existing dot-prefixed folder.
   if [ -d "$HOME/.$folder" ]; then
-    mv "$HOME/.$folder" "$HOME/.$folder.backup"
+    echo "Backing up ~/.$folder"
+    mv "$HOME/.$folder" "$HOME/.$folder.bak"
   fi
 
+  echo "Symlinking ~/.$folder to dotfiles/$folder"
   ln -s "$dotfiles_dir/$folder" "$HOME/.$folder"
 done
+
+echo
+
+
+# Creates dot-symlinks from $HOME to all files in the dotfiles/home folder.
+dotfiles_home_dir="$dotfiles_dir/home"
+files_link_with_dots=`ls -1 $dotfiles_home_dir`
 
 for file in $files_link_with_dots; do
   # Back up the existing dotfile.
   if [ -e "$HOME/.$file" ]; then
-    mv "$HOME/.$file" "$HOME/.$file.backup"
+    echo "Backing up ~/.$file"
+    mv "$HOME/.$file" "$HOME/.$file.bak"
   fi
 
+  echo "Symlinking ~/.$file to dotfiles/home/$file"
   ln -s "$dotfiles_home_dir/$file" "$HOME/.$file"
 done
 
@@ -41,11 +92,16 @@ done
 # Update the git submodules in the dotfiles folder.
 # Ref: http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
 if hash git 2>/dev/null; then
+  echo
+  echo "Initializing all the git submodules."
+  echo
+
   cd $dotfiles_dir
   git submodule init
   git submodule update
   cd -
 else
+  echo
   echo "Install git to initialize the submodules. Then run:"
   echo "  git submodule init"
   echo "  git submodule update"
@@ -54,6 +110,6 @@ fi
 
 echo
 echo "Bootstrap complete."
-echo "Check .backup files for .zshrc, .bashrc, .profile, .bash_profile, to see if there"
-echo "are config commands in those files that need to be loaded as well."
+echo "Check .bak files to see if there are any commands there that need to be copied."
+ls .*.bak
 echo
