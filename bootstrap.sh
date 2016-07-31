ddc14a0baf1db7f166be274254d56831a5564bd8
William Martin <awmartin@gmail.com>
2016-07-31 14:12:30 -0400

    Separated shell config from env variables

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# Creates symlinks from $HOME into dotfiles.
+# TODO: Maybe this should append source commands to existing files instead?
+
 dotfiles_dir="$HOME/dotfiles"
 dotfiles_home_dir="$dotfiles_dir/home"
 
 toplevel_folders_link_without_dots=( "bin" )
-toplevel_folders_link_with_dots=( "vim" "oh-my-zsh" )
+toplevel_folders_link_with_dots=( "vim" "oh-my-zsh" "env" "shell" )
 files_link_with_dots=`ls -1 $dotfiles_home_dir`
 
 for folder in $toplevel_folders_link_without_dots; do
@@ -36,6 +39,7 @@ done
 
 
 # Update the git submodules in the dotfiles folder.
+# Ref: http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
 if hash git 2>/dev/null; then
   cd $dotfiles_dir
   git submodule init
@@ -47,3 +51,9 @@ else
   echo "  git submodule update"
   echo "from the dotfiles directory"
 fi
+
+echo
+echo "Bootstrap complete."
+echo "Check .backup files for .zshrc, .bashrc, .profile, .bash_profile, to see if there"
+echo "are config commands in those files that need to be loaded as well."
+echo
