fe52520cd57d7dfaa1586b724ccf49a32638753d
William Martin <awmartin@anomnalus.com>
2021-11-16 22:05:43 -0500

    Added some install scripts

---
+++ homebrew/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# https://brew.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
---
+++ nvm/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# https://github.com/nvm-sh/nvm#installing-and-updating
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | /bin/bash
