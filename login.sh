67fd24413e39a5e381adfcdbf4d2d23873447395
William Martin <awmartin@anomalus.com>
2021-11-27 23:44:01 -0500

    Updates for bash support

--- login.sh
+++ login.sh
@@ -3,24 +3,31 @@
 # Get all the config variables needed.
 source $HOME/dotfiles/config.sh
 
-tools=( $dotfiles_dir/* )
-for tool_dir in $tools; do
-  detect_sh="$tool_dir/detect.sh"
+function run_login_scripts {
+  tools=( "$dotfiles_dir/*" )
+  for tool_dir in $tools; do
+    detect_sh="$tool_dir/detect.sh"
 
-  tool_exists=false
-  if [ -s $detect_sh ]; then
-    tool_exists=$(source $detect_sh)
-  fi
-
-  if $tool_exists; then
-    login_sh="$tool_dir/login.sh"
-    if [ -s $login_sh ]; then
-      source $login_sh
+    tool_exists=false
+    if [ -s $detect_sh ]; then
+      tool_exists=$(source $detect_sh)
     fi
 
-    login_platform_sh="$tool_dir/login.$platform.sh"
-    if [ -s $login_platform_sh ]; then
-      source $login_platform_sh
+    if $tool_exists; then
+      login_sh="$tool_dir/login.sh"
+      if [ -s $login_sh ]; then
+        source $login_sh
+      fi
+
+      login_platform_sh="$tool_dir/login.$platform.sh"
+      if [ -s $login_platform_sh ]; then
+        source $login_platform_sh
+      fi
     fi
-  fi
-done
+  done
+}
+
+if $is_login_shell
+then
+  run_login_scripts
+fi
